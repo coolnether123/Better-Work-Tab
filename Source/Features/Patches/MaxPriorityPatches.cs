ef9ba8241f08362a7c91714ce51b083eea59f4d6
Caspian Tyler <89215235+TreadTheDawnGames@users.noreply.github.com>
2026-02-28 15:47:06 -0500

    Transpile PawnColumnWorker_WorkPriority.HeaderClicked

--- Source/Features/Patches/MaxPriorityPatches.cs
+++ Source/Features/Patches/MaxPriorityPatches.cs
@@ -174,12 +174,95 @@ namespace Better_Work_Tab.Features.RaisedPriorityMaximum
     }
 
 
-    // Transpile PawnColumnWorker_WorkPriority.HeaderClicked(Rect headerRect, PawnTable table) to support priorities greater than 4
-    // line number: 178 in PawnColumnWorker_WorkPriority.cs
 
-    // Transpile Pawn_WorkSettings.SetPriority(WorkTypeDef w, int priority) to support priorities greater than 4
-    // line number: 160 in Pawn_WorkSettings.cs
+    [HarmonyPatch(typeof(PawnColumnWorker_WorkPriority), nameof(PawnColumnWorker_WorkPriority.HeaderClicked))]
+    public static class Patch_PawnColumnWorker_WorkPriority_HeaderClicked
+    {
+        // Transpile PawnColumnWorker_WorkPriority.HeaderClicked(Rect headerRect, PawnTable table) to support priorities greater than 4
+        // line number: 178 in PawnColumnWorker_WorkPriority.cs
 
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            //init the variable to hold the index of the opcode we want to start from
+            int[] getPriorityMethodIndexes = [-1, -1];
+
+            //convert the instructions to a list for easy manipulation
+            var codes = new List<CodeInstruction>(instructions);
+
+            //find the index of the opcode we want to start from by checking each instruction's operand
+            for (var i = 0; i < codes.Count; i++)
+            {
+                var operand = codes[i].operand as MethodInfo;
+                if (operand == AccessTools.PropertyGetter(typeof(Event), "button"))
+                {
+                    if (getPriorityMethodIndexes[0] == -1)
+                    {
+                        Log.Message("-------");
+                        Log.Message("found at: " + i.ToString());
+                        getPriorityMethodIndexes[0] = i;
+                    }
+                    else
+                    {
+                        Log.Message("found second get priority method at index: " + i.ToString());
+                        getPriorityMethodIndexes[1] = i;
+                        break;
+                        //Found both, so exit the loop
+                    }
+                }
+            }
+            if (getPriorityMethodIndexes[0] > -1 && getPriorityMethodIndexes[1] > -1)
+            {
+                Log.Message("first result: " + getPriorityMethodIndexes[0].ToString());
+                Log.Message("second result: " + getPriorityMethodIndexes[1].ToString());
+
+                int firstOpcodeIndex = getPriorityMethodIndexes[0] + 12;
+
+                Log.Message("first opcode: " + codes[firstOpcodeIndex].ToString());
+                Log.Message("second opcode: " + codes[getPriorityMethodIndexes[1] + 10].ToString());
+
+
+                Log.Message("first result opcode before: " + codes[firstOpcodeIndex].ToString());
+                Log.Message("maxPriorityInt: " + BetterWorkTabMod.Settings.maxPriorityInt.ToString());
+
+                //Replace the opcode that loads the constant value for the max priority (originally 4) with our mod setting value
+                codes[firstOpcodeIndex] = new CodeInstruction(OpCodes.Ldc_I4_S, BetterWorkTabMod.Settings.maxPriorityInt);
+                
+                codes.Insert(getPriorityMethodIndexes[0] + 2, CodeInstruction.Call(() => DebugMessage())); // Insert a NOP to preserve instruction offsets for debugging/logging purposes
+
+                Log.Message("first result opcode after: " + codes[firstOpcodeIndex].ToString());
+
+                //Also replace the second occurrence of the max priority constant in the method
+                codes[getPriorityMethodIndexes[1] + 10] = new CodeInstruction(OpCodes.Ldc_I4_S, BetterWorkTabMod.Settings.maxPriorityInt);
+
+            }
+            else
+            {
+                Log.Message("Did not find the expected opcodes to modify in PawnColumnWorker_WorkPriority.HeaderClicked. Transpiler may need to be updated if the method's implementation has changed.");
+            }
+
+
+            return codes.AsEnumerable();
+        }
+        public static void DebugMessage()
+        {
+            Log.Message("Debug message from Patch_PawnColumnWorker_WorkPriority_HeaderClicked");
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_WorkSettings), nameof(Pawn_WorkSettings.SetPriority))]
+    public static class PatchPawn_WorkSettings_SetPriority
+    {
+        // Transpile Pawn_WorkSettings.SetPriority(WorkTypeDef w, int priority) to support priorities greater than 4
+        // line number: 160 in Pawn_WorkSettings.cs
+        
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions;
+        }
+
+    }
 
 
 }
