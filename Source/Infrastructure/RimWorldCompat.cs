000ae0971d3ccea2060ec6fdb1c2dcbf34076628
coolnether123 <32144172+coolnether123@users.noreply.github.com>
2026-06-24 21:41:04 -0500

    Apply RimWorld 0.16 compatibility shims for v2.0 beta
    Apply safe mechanical replacements from the cascade compatibility preflight before building this support branch.

--- Source/Infrastructure/RimWorldCompat.cs
+++ Source/Infrastructure/RimWorldCompat.cs
@@ -894,7 +894,7 @@ namespace Better_Work_Tab
                 Directory.CreateDirectory(path);
                 return path;
 #else
-                return GenFilePaths.ConfigFolderPath;
+                return Better_Work_Tab.GenFilePathsCompat.ConfigFolderPath;
 #endif
             }
         }
@@ -974,7 +974,7 @@ namespace Better_Work_Tab
 #if v0_16
             Scribe_Values.LookValue(ref value, label, defaultValue, forceSave);
 #else
-            Scribe_Values.Look(ref value, label, defaultValue, forceSave);
+            Better_Work_Tab.ScribeCompat.LookValue(ref value, label, defaultValue, forceSave);
 #endif
         }
 
@@ -987,7 +987,7 @@ namespace Better_Work_Tab
 #if v0_16
             Scribe_Collections.LookList(ref list, label, lookMode, ctorArgs);
 #else
-            Scribe_Collections.Look(ref list, label, lookMode, ctorArgs);
+            Better_Work_Tab.ScribeCompat.LookCollection(ref list, label, lookMode, ctorArgs);
 #endif
         }
 
@@ -1037,7 +1037,7 @@ namespace Better_Work_Tab
                 }
             }
 #else
-            Scribe_Collections.Look(ref dictionary, label, keyLookMode, valueLookMode);
+            Better_Work_Tab.ScribeCompat.LookCollection(ref dictionary, label, keyLookMode, valueLookMode);
 #endif
         }
 
@@ -1055,7 +1055,7 @@ namespace Better_Work_Tab
 #if v0_16
             Scribe_Deep.LookDeep(ref target, label, ctorArgs);
 #else
-            Scribe_Deep.Look(ref target, label, ctorArgs);
+            Better_Work_Tab.ScribeCompat.LookDeep(ref target, label, ctorArgs);
 #endif
         }
 
