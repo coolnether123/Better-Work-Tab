761932144305ce34cb890d32ba3223dc62ca1194
coolnether123 <32144172+coolnether123@users.noreply.github.com>
2026-08-12 00:31:32 -0500

    Clarify optional compatibility settings

--- Source/Mod Support/Mods/Fluffy WorkTab/FluffyWorkTabGateway.cs
+++ Source/Mod Support/Mods/Fluffy WorkTab/FluffyWorkTabGateway.cs
@@ -1481,14 +1481,13 @@ namespace Better_Work_Tab.ModSupport.Mods.FluffyWorkTab
                         new SettingDefinition
                         {
                             Id = FluffyStyleTopButtons,
-                            ParentId = FluffyStyleFeatures,
+                            ParentId = CompatFluffyWorkTabOwnership,
                             FieldName = nameof(BetterWorkTabSettings.showFluffyStyleTopButtons),
                             Label = "Show top controls",
                             Tooltip = "When Fluffy Work Tab is installed, show its familiar icon controls for manual priorities, time schedules, and expanding or collapsing sub-work jobs.",
                             SearchKeywords = FluffyControlsSearchKeywords,
                             Type = SettingType.Bool,
                             DefaultValue = DefaultSettings.showFluffyStyleTopButtons,
-                            VisibleWhen = _ => FluffyWorkTabGateway.IsPresent,
                             ShowInSimpleView = false,
                             ShowInAdvancedView = true,
                             SortOrder = 1
@@ -1511,14 +1510,13 @@ namespace Better_Work_Tab.ModSupport.Mods.FluffyWorkTab
                         new SettingDefinition
                         {
                             Id = FluffyStyleScheduleAssigner,
-                            ParentId = FluffyStyleFeatures,
+                            ParentId = CompatFluffyWorkTabOwnership,
                             FieldName = nameof(BetterWorkTabSettings.enableFluffyScheduleAssigner),
                             Label = "Use hour-selection scheduler",
                             Tooltip = "Use Fluffy's original bottom hour selector: choose hours, then click normal priority boxes to assign those hours. This option requires Fluffy Work Tab because it uses Fluffy's scheduler assets.",
                             SearchKeywords = FluffyScheduleSearchKeywords,
                             Type = SettingType.Bool,
                             DefaultValue = DefaultSettings.enableFluffyScheduleAssigner,
-                            VisibleWhen = _ => FluffyWorkTabGateway.IsPresent,
                             ShowInSimpleView = false,
                             ShowInAdvancedView = true,
                             SortOrder = 3
@@ -1526,7 +1524,7 @@ namespace Better_Work_Tab.ModSupport.Mods.FluffyWorkTab
                         new SettingDefinition
                         {
                             Id = CompatFluffyWorkTabOwnership,
-                            Label = "Installed-mod ownership",
+                            Label = "Fluffy Work Tab compatibility",
                             Tooltip = "Choose which compatible mod runs the Work tab. When Fluffy Work Tab is installed, also choose whether its columns remain visible.",
                             SearchKeywords = FluffyOwnershipSearchKeywords,
                             Type = SettingType.Header,
@@ -1574,7 +1572,6 @@ namespace Better_Work_Tab.ModSupport.Mods.FluffyWorkTab
                             SearchKeywords = FluffyOwnershipSearchKeywords,
                             Type = SettingType.Bool,
                             DefaultValue = DefaultSettings.showExternalWorkTabColumns,
-                            VisibleWhen = _ => FluffyWorkTabGateway.IsPresent,
                             Suppressions = new List<SettingSuppression>
                             {
                                 CreateWorkTabOwnedByFluffySuppression(WorkTabOwnedByFluffyReason)
